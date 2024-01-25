ea5e6b24d79d6ac99f030e86b61d26fbc9f01a0a
ainoruo <aino.ruohola@metropolia.fi>
2024-01-25 12:31:43 +0200

    commit

---
+++ HARJOITUS/bmi.js
@@ -0,0 +1,102 @@
+"use strict";
+
+let lowBmi = `Jos painoindeksi on alle 18,5, se merkitsee liiallista laihuutta. Sen syynä voi olla jokin pitkällinen sairaus tai laihuushäiriö eli anoreksia. Jos varsinaista sairautta ei ole, mutta painoindeksi on laskenut alle 18,5:n, pitää hakeutua lääkäriin. Jos paino muutamassa kuukaudessa on laskenut yli 20:n tasolta reilusti, on varminta mennä lääkäriin jo painoindeksin lähestyessä 19:ää.`;
+
+let normalBmi = `Normaaliksi on valittu se painoindeksin alue, jossa ihmisen terveys on parhaimmillaan. Normaali painoindeksin alue on välillä 18,5–25. Jos painoindeksi on pienempi kuin 18,5 tai suurempi kuin 25, sairauksien vaara suurenee. Painoindeksiä voidaan käyttää 18 vuoden iästä lähtien.`;
+
+let highBmi = `Kun painoindeksi ylittää 25, ollaan liikapainon puolella. Liikakilojen määrä voi vaihdella erittäin paljon, muutamasta kilosta moniin kymmeniin kiloihin. Siksi on hyödyllistä täsmentää, kuinka suuresta ylipainosta on kyse.`;
+
+//BOM tiedot
+
+let age = 14;
+
+let teksti = "Moi, olen " + age + " ikäinen";
+console.log(teksti);
+
+let teksti2 = `Moi olen yhä vuoden ${age} ikäinen`;
+console.log(teksti2);
+
+window.console.log("Moi, tämä toimii");
+console.log("Moro taas");
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+
+//tietojen haku
+//hakee ensimmäisen minkä löytää
+const analysis = document.querySelector(".analysis");
+console.log(analysis);
+console.log(analysis.innerText);
+console.log(analysis.innerHTML);
+
+
+//tietojen muokkaus
+analysis.textContent= 'Terveppä terve🩰';
+// analysis.textContent = normalBmi;
+
+
+//kaikki
+const allP = document.querySelectorAll("p");
+console.log(allP);
+
+for (const p of allP) {
+  console.log("P elementin korkeus");
+  console.log(p.offsetHeight);
+}
+
+//eventit
+
+document.addEventListener('keydown', function(e) {
+    console.log(e.key);
+});
+
+
+const nappula = document.querySelector('.calculate');
+nappula.addEventListener('click', function (evt) {
+    console.log('Nappulaa klikattiin');
+    console.log(evt);
+
+
+//Yleensä kun UI:ssa saadaan arvo se on lähtökohtaisesti STRING muotoinen    
+    const weight = Number(document.getElementById('weight').value);
+    const height = Number(document.getElementById('height').value);
+
+    console.log(typeof weight);
+
+    let yht = weight + height;
+    console.log(yht);
+
+    if (!weight || !height) {
+        analysis.textContent = 'Muista lisätä numerot!!';
+    } else {
+        resettifunktio();
+        bmiLaskuri(weight, height);
+    }
+});
+
+function bmiLaskuri(weight, height) {
+    console.log('Lasketaan bmi');
+    let bmi = (weight / ((height * height) / 10000)).toFixed(1);
+    console.log(bmi);
+    document.querySelector('.bmi-score').textContent = bmi;
+
+    if(bmi < 19){
+        console.log('Alipaino');
+        document.querySelector('.analysis').textContent = lowBmi;
+        document.querySelector('.bmi0-19').classList.add('lowBmi');
+    } else if (bmi > 25) {
+        console.log('Ylipaino');
+        document.querySelector('.analysis').textContent = highBmi;
+        document.querySelector('.bmi25-30').classList.add('highBmi');
+    } else {
+        console.log('Normaali paino');
+        document.querySelector('.analysis').textContent = normalBmi;
+        document.querySelector('.bmi19-25').classList.add('normalBmi');
+    }
+}
+
+function resettifunktio() {
+    //tyylien resetointi
+}
+
+
